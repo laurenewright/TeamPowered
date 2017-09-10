7061b8055dbd0f5d94543bc0d1a5166afd15e969
lauren wright <wright.lauren20@gmail.com>
2017-09-10 14:08:52 -0400

    test

--- TeamPowered/TeamPowered/Person.cs
+++ TeamPowered/TeamPowered/Person.cs
@@ -11,5 +11,6 @@ namespace TeamPowered
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string Address { get; set; }
+        public string NewAddress { get; set; }
     }
 }
